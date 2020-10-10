2cd3f3c8b21933c8463eb70320e2b1258f8f84ce
pablo marino <pablo-n-m@hotmail.com>
2020-10-10 01:48:43 -0300

    setup script for training xl model

--- finetune.py
+++ finetune.py
@@ -7,7 +7,7 @@ TEST_FILE = FILE_PATH + "/input/wiki.test.raw"
 
 
 # gpt2 is the 117M version, gpt2-xl is the 1.5B version
-MODEL_PATH = "gpt2"# "gpt2-xl"
+MODEL_PATH = "gpt2-xl"#"gpt2"
 MODEL_TYPE = "gpt2"
 
 # IF WANTED TO IMPROVE RESULTS: increase per_device_train_batch_size and num_train_epochs, the firt one will require higher memory GPUs and the last more time
@@ -15,7 +15,7 @@ MODEL_TYPE = "gpt2"
 def finetune():
     # more info: https://github.com/huggingface/transformers/tree/master/examples/language-modeling
     # flags info: https://github.com/huggingface/transformers/blob/master/src/transformers/training_args.py
-    arguments = "--output_dir=output --model_type={} --model_name_or_path={} --do_train --train_data_file={} --per_device_train_batch_size=1 --num_train_epochs=2 --fp16".format(MODEL_TYPE, MODEL_PATH, TRAIN_FILE)
+    arguments = "--output_dir=output --overwrite_output_dir --model_type={} --model_name_or_path={} --do_train --train_data_file={} --per_device_train_batch_size=1 --num_train_epochs=3 --fp16".format(MODEL_TYPE, MODEL_PATH, TRAIN_FILE)
     os.system("python " + FILE_PATH +"/run_language_modeling.py " + arguments)
 
 
